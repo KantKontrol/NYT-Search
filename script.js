3d50ae670be0f392c7c4d3f7da2ccce4cb14126d
Andrew Bautista <andrewbautista85@gmail.com>
2020-01-28 21:32:06 -0500

    fixed response data

--- script.js
+++ script.js
@@ -4,17 +4,26 @@ $("button").click(function (e) {
 
     var APIKey = "q7cYqjgdTWrVYAfzuD6z9MIXvZOKKHzv"
 
-    var userQuery = "&q=" + $("#searchTerm").val() //key word input of user
+    var userQuery = "&q=" + $("#searchTerm").val()
+    console.log(userQuery) //key word input of user
 
     var startYear = "&begin_date=" + $("#startYear").val()//userinput
         + "0101"
-
+    console.log(startYear)
     var endYear = "&end_date=" + $("#endYear").val()//userinput 
         + "1231"
+    console.log(endYear);
 
+    if ($("#startYear").val() === "" && $("#endYear").val() === "") {
+        var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + APIKey + userQuery
+    }
 
-
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + APIKey + userQuery + startYear + endYear
+    else if ($("#endYear").val() === "") {
+        var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + APIKey + userQuery + startYear
+    }
+    else {
+        var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + APIKey + userQuery + startYear + endYear
+    }
     // Parameters go heres
 
 
@@ -23,21 +32,32 @@ $("button").click(function (e) {
         method: "GET"
     }).then(function (response) {
 
-        var numOfRecords = $("numOfArticles").val();
-
-        for (var i = 0; i < numOfRecords; i++) {
+        var numOfRecords = $("#numOfArticles").val();
+        if (numOfRecords === "") {
 
             var articleDump = $("#articleDump")
-            var headline = $("<div>").html("<a href=" + response[i].web_url + ">" + response[i].abstract + "</a>")
-            var description = $("<div>").text(response[i].lead_paragraph)
+            var headline = $("<div>").html("<a href=" + response.response.docs[0].web_url + "></strong>" + response.response.docs[0].abstract + "</strong></a>")
+            var description = $("<div>").text(response.response.docs[0].lead_paragraph)
 
 
             articleDump.prepend(description);
             articleDump.prepend(headline);
 
+        } else {
+            for (var i = 0; i < numOfRecords; i++) {
 
+                var articleDump = $("#articleDump")
+                var headline = $("<div>").html("<a href=" + response.response.docs[i].web_url + "><strong>" + response.response.docs[i].abstract + "</a>")
+                var description = $("<div>").text(response.response.docs[i].lead_paragraph)
+
+
+                articleDump.prepend(description);
+                articleDump.prepend(headline);
+
+
+            }
         }
 
-
     })
+
 })
