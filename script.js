775858c652a1e285e10c67807d79ec6780f04e0c
Andrew Bautista <andrewbautista85@gmail.com>
2020-01-28 20:43:59 -0500

    added start year and end year variables

--- script.js
+++ script.js
@@ -2,9 +2,15 @@ var APIKey = q7cYqjgdTWrVYAfzuD6z9MIXvZOKKHzv
 
 var userQuery = "&q=" + ""//key word input of user
 
+var startYear = "&begin_date=" + ""//userinput
+    + "0101"
+
+var endYear = "&end_date=" + "" //userinput 
+    + "1231"
 
 
-var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?apikey=" + APIKey + search + article
+
+var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?apikey=" + APIKey + userQuery + startYear + endYear
 // Parameters go heres
 ""
 
@@ -18,6 +24,7 @@ $.ajax({
 
         for (var i = 0; i < numOfRecords, i++) {
 
+            // Where will this show up?
             response[i];
 
         }
