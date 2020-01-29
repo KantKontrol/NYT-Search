df7b52eb6b5354a30518f30bb5bcb54843d6d7ff
Andrew Bautista <andrewbautista85@gmail.com>
2020-01-28 20:10:15 -0500

    added apikey and changed to fq in URL

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
-    // Parameters go heres
-    ""
+var APIKey = q7cYqjgdTWrVYAfzuD6z9MIXvZOKKHzv
+
+var search = ""//input of user
+
+var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?apikey=" + APIKey + "&fq=" + search
+// Parameters go heres
+""
 
 $.ajax({
     url: queryURL,
